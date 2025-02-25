95721f08d04a24b8d9755fd68f6583389ab6c0a9
Brendan Kersey <bmkersey@gmail.com>
2025-02-25 15:00:46 -0500

    added pushable blocks and puzzle buttons. Also made base the blocks and buttons as bases, so i can customize them later

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -3,11 +3,14 @@ class_name Player
 
 @export var hp: int = 100
 @export var move_speed: float = 100
+@export var push_strength:float = 300
 
 func _ready() -> void:
-	position = SceneManager.player_spawn_pos
+	if SceneManager.player_spawn_pos != Vector2(0,0):
+		position = SceneManager.player_spawn_pos
+	
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = move_vector * move_speed
@@ -21,5 +24,14 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play("move_up")
 	else:
 		$AnimatedSprite2D.stop()
-	
+		
+	#get last collision
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	#check its a block
+	if collision:
+		var collider_node = collision.get_collider()
+	#if its a block push it
+		if collider_node.is_in_group("pushable"):
+			var collision_normal: Vector2 = collision.get_normal()
+			collider_node.apply_central_force(-collision_normal * push_strength)
 	move_and_slide()
